93eaf17e5b54085d78276bb2c741476aa52eebe7
MichaelGmx <474542562@qq.com>
2022-05-24 11:14:17 +0800

    pwa

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import WeatherApp from './WeatherApp';
 // import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
 // 全局样式
 import './index.css';
@@ -18,3 +19,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+serviceWorker.register();
